ecd63641326cb4f09eb1bbed51bb42c9a65a8120
minnehkamau <minnieches08@gmail.com>
2022-12-03 11:45:09 +0300

    complete assingnment

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
 // Write your code here!
+//removing tag main#main
+const element = document.getElementById("main");
+element.remove("main");
+
+//creating h1 element
+const newHeader = document.createElement("h1");
+
+//creating h1 id
+newHeader.id= "victory"
+    
+
+//creating innerHtml inside h1
+newHeader.innerHTML = "YOUR-NAME is the champion"
+
+
